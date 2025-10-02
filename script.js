b885a358973ab625fd7a40ddf9fbfb247bf54017
El Thiro <thirotrece@gmail.com>
2025-10-02 18:46:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const sidebar = document.getElementById("sidebar");
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+// Mostrar la sección de proyectos al hacer clic en el menú
+document.getElementById('enlace-proyectos').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.getElementById('proyectos').style.display = 'block';
+});
