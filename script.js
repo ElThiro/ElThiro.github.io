3175cd83757782e4345a2ba7fd8b44abea08095a
El Thiro <thirotrece@gmail.com>
2025-10-03 13:20:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,8 +37,4 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener('click', (e) => {
   const isClickInsideSidebar = sidebar.contains(e.target);
   const isClickOnMenuBtn = menuBtn.contains(e.target);
-
-  if (!isClickInsideSidebar && !isClickOnMenuBtn) {
-    sidebar.classList.remove('active');
-  }
 });
